3399fad0c4870474efb826d01dbb24d70d338d81
Rahi Reyaz <rahiryazdar@gmail.com>
2024-06-28 20:32:13 +0530

    Started city Repo and added unique constraint

---
+++ src/repository/city-repository.js
@@ -0,0 +1,27 @@
+const { city } = require("../models/index");
+
+class CityRepository {
+    async createCity({ name }) {
+        try{
+            const city = await City.create({ name });
+            return city;
+        } catch(error) {
+            throw {error};
+        }
+    }
+
+    async deleteCity(cityId){
+        try{
+            await City.destroy({
+                where : {
+                    id:cityId
+                }
+            });
+        } catch(error) {
+            throw {error};
+        }
+    }
+
+}
+
+module.export = CityRepository;
