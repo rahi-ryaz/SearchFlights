e3dba2f742e603ea6590d1c9f3e85bc67f95b31b
Rahi Reyaz <rahiryazdar@gmail.com>
2024-07-04 15:24:35 +0530

    Airports Model Added

---
+++ src/migrations/20240704093118-create-airport.js
@@ -0,0 +1,42 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Airports', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      address: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      cityId: {
+        type: Sequelize.INTEGER,
+        ONdELETE : "CASCADE",
+        references:{
+          model:'Cities',
+          Key: 'Id',
+          as : 'cityId'
+        },
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Airports');
+  }
+};
