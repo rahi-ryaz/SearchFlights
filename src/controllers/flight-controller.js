53f8eab3b0c4a6d601039966df79d8f08721185e
Rahi Reyaz <rahiryazdar@gmail.com>
2024-07-07 20:29:48 +0530

    Added error code enums

--- src/controllers/flight-controller.js
+++ src/controllers/flight-controller.js
@@ -1,4 +1,5 @@
 const {FlightService} = require("../services/index");
+const { SuccessCodes } = require("../utils/error-codes");
 
 const flightService = new FlightService();
 
@@ -16,7 +17,7 @@ const create = async(req,res) => {
         }
 
         const flight = await flightService.createFlight(flightRequestData);
-        return res.status(201).json({
+        return res.status(SuccessCodes.CREATED).json({
             data:flight,
             success:true,
             err:true,
@@ -36,7 +37,7 @@ const create = async(req,res) => {
 const getAll = async (req,res)=>{
     try{
         const response=await flightService.getAllFlightData(req.query);
-        return res.status(200).json({
+        return res.status(SuccessCodes.OK).json({
             data:response,
             success:true,
             err:{},
--- src/middlewares/flight-middlewares.js
+++ src/middlewares/flight-middlewares.js
@@ -1,3 +1,5 @@
+const { ClientErrorCodes } = require("../utils/error-codes");
+
 const validateCreateFlight = (req,res,next) => {
     if(
         !req.body.flightNumber ||
@@ -9,7 +11,7 @@ const validateCreateFlight = (req,res,next) => {
         !req.body.price
         
     ) {
-           return res.status(400).json({
+           return res.status(ClientErrorCodes.BAD_REQUEST).json({
             data: {},
             success: false,
             message: 'Invalid request body for create flight',
